2fdf8ccb38436d097da90d458beaa497cba533f2
Miguel Ike Dumancas <ikedumancas@gmail.com>
2016-01-22 16:57:55 +0800

    added transaction_id and timestamp on model

--- website/models.py
+++ website/models.py
@@ -3,11 +3,13 @@ from django.db import models
 
 class Order(models.Model):
     email = models.EmailField()
+    transaction_id = models.CharField(max_length=120)
     cvcode = models.IntegerField("CV Code")
     expirem = models.IntegerField("Expire Month")
     expirey = models.IntegerField("Expire Year")
     cardnumber = models.CharField("Card Number", max_length=50)
     price = models.DecimalField("Price", max_digits=5, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
     class Meta:
         verbose_name = "Order"
