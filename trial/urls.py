ad1470d981243dc1e2b50ef840508fb17fba8518
Miguel Ike Dumancas <ikedumancas@gmail.com>
2016-01-22 16:33:44 +0800

    add thanks page

--- trial/urls.py
+++ trial/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', 'website.views.index'),
+    url(r'^thanks/$', 'website.views.thanks', name='thanks'),
     url(r'^admin/', admin.site.urls),
 ]
