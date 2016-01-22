72b33819ca50e21fe68cadfa4cc64e946a867eca
Miguel Ike Dumancas <ikedumancas@gmail.com>
2016-01-22 16:42:31 +0800

    created Order model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    email = models.EmailField()
+    cvcode = models.IntegerField("CV Code")
+    expirem = models.IntegerField("Expire Month")
+    expirey = models.IntegerField("Expire Year")
+    cardnumber = models.CharField("Card Number", max_length=50)
+    price = models.DecimalField("Price", max_digits=5, decimal_places=2)
+
+    class Meta:
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
+
+    def __str__(self):
+        return self.email
+
+    def __unicode__(self):
+        return self.email
