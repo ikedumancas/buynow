113a93480e62221526f0e38caa8cac14886c5cd1
Miguel Ike Dumancas <ikedumancas.official@gmail.com>
2016-02-02 04:23:20 +0800

    modified model to save values returned from braintree and removed saving creditcard number

--- website/models.py
+++ website/models.py
@@ -4,11 +4,9 @@ from django.db import models
 class Order(models.Model):
     email = models.EmailField()
     transaction_id = models.CharField("Transaction ID", max_length=120)
-    cvcode = models.IntegerField("CV Code")
-    expirem = models.IntegerField("Expire Month")
-    expirey = models.IntegerField("Expire Year")
-    cardnumber = models.CharField("Card Number", max_length=50)
     price = models.DecimalField("Price", max_digits=5, decimal_places=2)
+    card_type = models.CharField("Card Type", max_length=120)
+    last_4 = models.CharField("Last 4 Digits", max_length=50)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
     class Meta:
